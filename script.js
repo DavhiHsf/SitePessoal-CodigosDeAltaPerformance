9e2d53b4ce85b287a535e14ce08ef0e6095ca9fe
Davi Henrique S. de Farias <121799854+DavhiHsf@users.noreply.github.com>
2024-05-22 18:39:05 -0300

    Add files via upload
    Segunda versão

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 function navigateTo(url) {
     window.location.href = url;
     
-}
+} 
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.getElementById('darkmode').addEventListener('click', function() {
+//     document.body.classList.toggle('darkmode');
     
-    const ajustarFonteBtn = document.getElementById('ajustar-fonte-btn');
-    const fontAdjuster = document.getElementById('ajustar-fonte');
-    const increaseFontButton = document.getElementById('increaseFont');
-    const decreaseFontButton = document.getElementById('decreaseFont');
-    const content = document.getElementById('content');
-    
-    let currentFontSize = 16; // Tamanho de fonte inicial
+//   });
 
-    ajustarFonteBtn.addEventListener('click', function() {
-        // Mostrar ou esconder o bloco flutuante
-        if (fontAdjuster.style.display === 'none' || fontAdjuster.style.display === '') {
-            const rect = ajustarFonteBtn.getBoundingClientRect();
-            fontAdjuster.style.display = 'block';
-            fontAdjuster.style.top = `${rect.bottom}px`;
-            fontAdjuster.style.left = `${rect.left}px`;
-        } else {
-            fontAdjuster.style.display = 'none';
-        }
-    });
-
-    increaseFontButton.addEventListener('click', function() {
-        currentFontSize += 2;
-        content.style.fontSize = `${currentFontSize}px`;
-    });
-
-    decreaseFontButton.addEventListener('click', function() {
-        currentFontSize -= 2;
-        content.style.fontSize = `${currentFontSize}px`;
-    });
-});
