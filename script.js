195ed947a81acef73693b458163d3292fa92ee4a
Davi Henrique S. de Farias <121799854+DavhiHsf@users.noreply.github.com>
2024-05-24 10:20:53 -0300

    Add files via upload
    terceira versão

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 function navigateTo(url) {
     window.location.href = url;
     
-} 
+    } 
 
-// document.getElementById('darkmode').addEventListener('click', function() {
-//     document.body.classList.toggle('darkmode');
+function darkmode() {
+    const htmlElement = document.documentElement;
+    const button = document.getElementById('darkmode')
+
+    htmlElement.classList.toggle('darkmode');
+
+  if (htmlElement.classList.contains('darkmode')) {
+    button.textContent = 'LightMode ☀️';
+    localStorage.setItem('theme', 'dark');
+
+  } 
+  
+  else {
+    button.textContent = 'DarkMode 🌕';
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+function fixartema() {
+    const theme = localStorage.getItem('theme');
+    const htmlElement = document.documentElement;
+    const button = document.getElementById('darkmode');
     
-//   });
+    if (theme === 'dark') {
+      htmlElement.classList.add('darkmode');
+      button.textContent = 'LightMode ☀️';
+    } else {
+      htmlElement.classList.remove('darkmode');
+      button.textContent = 'DarkMode 🌕';
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', fixartema);
+
+document.getElementById('darkmode').addEventListener('click', darkmode);
+
+
 
